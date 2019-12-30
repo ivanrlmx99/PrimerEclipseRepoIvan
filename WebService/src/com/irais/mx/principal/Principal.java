9ed6e0ddff079ce31a7c84d19f274080787fdd3c
ivanmx99 <ivanlaureano77gmail.com>
2019-12-30 00:17:13 -0600

    subo nuevo cambio 1216am

--- WebService/src/com/irais/mx/principal/Principal.java
+++ WebService/src/com/irais/mx/principal/Principal.java
@@ -10,14 +10,10 @@ public class Principal {
 	public static void main(String[] args) {
 		// TAqu� van los comentarios del codigo 
 		Endpoint.publish("http://localhost:1523/ws/ivanmx",new CalculadoraImpl());
-<<<<<<< HEAD
-		//Con esta anotacion se muestra en la url del navegador el web services 
+
 		
-		
-=======
-		
-		System.out.printl("Agregué este comentario desde la pagina de git desde internet....");
->>>>>>> branch 'master' of https://github.com/ivanrlmx99/PrimerEclipseRepoIvan.git
+		System.out.println("Agregué este comentario desde la pagina de git desde internet....");
+
 	}
 
 }
