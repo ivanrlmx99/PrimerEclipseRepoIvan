a53cbae84d363828264d4dcd2ccd00e3159a61d2
ivanmx99 <ivanlaureano77gmail.com>
2019-12-30 00:01:53 -0600

    agregue a las 12 un cambio 30 de diciembre.

--- WebService/src/com/irais/mx/principal/Principal.java
+++ WebService/src/com/irais/mx/principal/Principal.java
@@ -8,9 +8,10 @@ import com.irais.mx.impl.CalculadoraImpl;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TAquí van los comentarios del codigo 
 		Endpoint.publish("http://localhost:1523/ws/ivanmx",new CalculadoraImpl());
+		
+		
 	}
 
 }
